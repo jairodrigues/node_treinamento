cc2ce2711569e7c88009383facb29cc6598fd7cc
Jairo <jairo.goncalves90@gmail.com>
2018-04-01 22:45:01 -0300

    Aula 9

--- config/express.js
+++ config/express.js
@@ -6,6 +6,7 @@ var expressValidator = require('express-validator');
 module.exports = function() {
     var app = express();
 
+    app.use(express.static('./public'));
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
 
